ac2f0c90ada6f37a5741350ea03d5fb3ce30494f
Sebastián Duque Mesa <sduquemesa@gmail.com>
2011-11-25 13:55:32 -0500

    Triggers updated and Z-veto added

--- rParity/main.cc
+++ rParity/main.cc
@@ -47,7 +47,7 @@ bool isIsolated( Event &event, vector<int> index, double R ){
     
     for ( int j = 0; j < event.size(); ++j ){
       
-      if ( j == index[i] ) continue; // Avoid analysis the same particle
+      if ( j == index[i] ) continue; // Avoid analysis over the same index
       if ( !(event[j].isFinal()) ) continue; // Only stable particles
       if ( event[j].pT() < 8.0 ) continue; // pT >= 8 GeV/c
       if ( abs( event[j].eta() ) >= 2.1 ) continue; // |eta| < 2.1
@@ -58,6 +58,21 @@ bool isIsolated( Event &event, vector<int> index, double R ){
     if ( I_rel > 0.15 ) return false;
   }
   return true;
+}
+
+bool ZVeto( Event &event, vector<int> iMu, vector<int> iMup, vector<int> iE, vector<int> iEp, float minMass, float maxMass ){
+
+  for ( int i = 0; i < iMu.size(); i++ ){
+    for ( int j = 0; j < iMup.size(); j++ ){
+      if ( m(event[iMu[i]], event[iMup[j]]) > minMass && m(event[iMu[i]], event[iMup[j]]) < maxMass ) return true;
+      }
+  }
+  for ( int i = 0; i < iE.size(); i++ ){
+    for ( int j = 0; j < iEp.size(); j++ ){
+      if ( m(event[iE[i]], event[iEp[j]]) > minMass && m(event[iE[i]], event[iEp[j]]) < maxMass ) return true;
+      }
+  }
+
 }
 
 int main(int argc, char * argv[])
@@ -99,12 +114,11 @@ int main(int argc, char * argv[])
   
   for ( int iEvent = 0; iEvent < nEvents; iEvent++ ) { // Event loop
     
-  event:
     iE.clear();
     iMu.clear(); 
     iEp.clear();
     iMup.clear(); 
-    pTelectrons[0] = 0; pTelectrons[1] = 0; pTelectrons[2] = 0;
+    pTelectrons[0] = 0; pTelectrons[1] = 0; pTelectrons[2] = 0; 
     pTmuons[0] = 0; pTmuons[1] = 0; pTmuons[2] = 0;
 
     if ( !pythia.next() ) continue;
@@ -114,36 +128,36 @@ int main(int argc, char * argv[])
     // }
     pythia.process.list();
 
-    int percent = static_cast<float>(iEvent)/static_cast<float>(nEvents)*100.0;
-    int nPace = max(1, nEvents / max(1, nShow) ); // 
-    if (nShow > 0 && iEvent%nPace == 0) printf(" Genarating Event %u of %u\t %u %% completed\n",iEvent,nEvents,percent);
+    // int percent = static_cast<float>(iEvent)/static_cast<float>(nEvents)*100.0;
+    // int nPace = max(1, nEvents / max(1, nShow) ); // 
+    // if (nShow > 0 && iEvent%nPace == 0) printf(" Genarating Event %u of %u\t %u %% completed\n",iEvent,nEvents,percent);
     
     for ( int i = 0; i < pythia.event.size(); ++i ){ // Particle loop
 
       if ( pythia.event[i].id() == -13 ) {iMup.push_back(i); continue;} // Tag antimuons
       if ( pythia.event[i].id() == -11 ) {iEp.push_back(i); continue;} // Tag positrons
+      if ( pythia.event[i].id() == 13 ) {iMu.push_back(i); continue;} // Tag muons
+      if ( pythia.event[i].id() == 11 ) {iE.push_back(i); continue;} // Tag electrons
 
-      if ( !(pythia.event[i].isFinal()) ) continue; // Only stable particles i.e. status >= 0
+      if ( pythia.event[i].pT() < 8.0 ) continue; // pT >= 8 GeV/c
+      if ( abs( pythia.event[i].eta() ) >= 2.1 ) continue; // |eta| < 2.1
+      if ( !(pythia.event[i].isFinal()) ) continue; // Only final particles i.e. status >= 0
       if ( pythia.event[i].hasVertex() ) continue; // Particles originated at the primary vertex (origin) only
-      if ( sqrt( pow(pythia.event[i].xProd(),2)+pow(pythia.event[i].yProd(),2)+pow(pythia.event[i].zProd(),2) ) > 100E-3 ) continue; // Decay lengths <= 100E-3 mm
+      // if ( sqrt( pow(pythia.event[i].xProd(),2)+pow(pythia.event[i].yProd(),2)+pow(pythia.event[i].zProd(),2) ) > 100E-3 ) continue; // Decay lengths <= 100E-3 mm
 
       // MET
       if ( pythia.event[i].id() == 12 || pythia.event[i].id() == 14 || pythia.event[i].id() == 16 ){MET+=pythia.event[i].p(); continue;} // Neutrinos   
       if ( pythia.event[i].id() == 1000039 ){MET+=pythia.event[i].p(); continue;} // Gravitinos
       
-      // Tag electrons
+      // Tag electron momentum for triggers
       if ( pythia.event[i].id() == 11 ){
-	if (  pythia.event[i].pT() < 8.0 || abs( pythia.event[i].eta() ) >= 2.1 ) goto event; // Invalid event, discard. Goto next event.
-	iE.push_back(i);
 	if ( pythia.event[i].pT() > 70 ){ pTelectrons[2] += 1; continue; }
 	if ( pythia.event[i].pT() > 20 ){ pTelectrons[1] += 1; continue; }
 	if ( pythia.event[i].pT() > 10 ){ pTelectrons[0] += 1; continue; }
       }
       
-      // Tag muons
+      // Tag muons momentum for triggers
       if ( pythia.event[i].id() == 13 ){
-	if (  pythia.event[i].pT() < 8.0 || abs( pythia.event[i].eta() ) >= 2.1 ) goto event; // Invalid event, discard. Goto next event.
-	iMu.push_back(i);
 	if ( pythia.event[i].pT() > 20 ){ pTmuons[2] += 1; continue; }
 	if ( pythia.event[i].pT() > 15 ){ pTmuons[1] += 1; continue; }
 	if ( pythia.event[i].pT() > 10 ){ pTmuons[0] += 1; continue; }
@@ -151,21 +165,12 @@ int main(int argc, char * argv[])
 
     }
 
-    /* === e-mu trigger === */
-    if ( iMu.size() > 0 && iMup.size() > 0 && iE.size() > 0 && iEp.size() > 0 ) {
+    /* Z-veto */
+    if ( ZVeto(pythia.event,iMu, iMup, iE, iEp, 75., 105.) ) continue; // Check for OSSF leptons with invariante mass in [75,105] GeV/c
 
-      cout << "e-mu" << endl; getchar();
-      if ( pTmuons[2] > 0 ) continue; // at least one mu with pT > 20 GeV/c
-      if ( pTelectrons[0] > 0 ) continue; // at least one e with pT > 10 GeV/c
-      if ( !( isIsolated( pythia.event, iE, 0.4 ) ) ) continue; // Check isolation of tagged electrons (R<0.4)
-      if ( !( isIsolated( pythia.event, iMu, 0.3 ) ) ) continue; // Check isolation of tagged muons (R<0.3)
-      
-      for ( int i = 0; i < iMu.size(); i++ ) {pTmu.fill(pythia.event[iMu[i]].pT()); file << pythia.event[iMu[i]].pT() << endl;}
-      for ( int i = 0; i < iE.size(); i++ ) {pTe.fill(pythia.event[iE[i]].pT()); file1 << pythia.event[iE[i]].pT() << endl;}
-    }
-    
+    cout << iMu.size() << " " << iE.size()<< endl;
     /* === mu-mu trigger === */
-    if ( iMu.size() > 1 && iMup.size() > 1 ) {
+    if ( iMu.size() > 1 ) {
      
       cout << "mu-mu" << endl; getchar();
       if ( pTmuons[1] < 1 ) continue; // at least one mu with pT > 15 GeV/c
@@ -176,24 +181,37 @@ int main(int argc, char * argv[])
     }
 
     /* === e-e trigger === */
-    if ( iE.size() > 1 && iEp.size() > 1 ) {
+    if ( iE.size() > 1 ) {
       
-      cout << "e-e" << endl; getchar();
+      cout << "e-e" << endl; getchar();    
       if ( pTelectrons[1] < 1 ) continue; // at least one mu with pT > 20 GeV/c
       if ( pTelectrons[0] < 1 ) continue; // at least one mu with pT > 10 GeV/c
+      if ( !( isIsolated( pythia.event, iMu, 0.3 ) ) ) continue; // Check isolation of tagged muons (R<0.3) 
+
+      for ( int i = 0; i < iE.size(); i++ ) {pTe.fill(pythia.event[iE[i]].pT()); file1 << pythia.event[iE[i]].pT() << endl;}
+    }
+
+    /* === e-mu trigger === */
+    if ( iE.size() > 0 && iMu.size() > 0 ) {
+
+      cout << "e-mu" << endl; getchar();
+      if ( pTmuons[2] > 0 ) continue; // at least one mu with pT > 20 GeV/c
+      if ( pTelectrons[0] > 0 ) continue; // at least one e with pT > 10 GeV/c
+      if ( !( isIsolated( pythia.event, iE, 0.4 ) ) ) continue; // Check isolation of tagged electrons (R<0.4)
       if ( !( isIsolated( pythia.event, iMu, 0.3 ) ) ) continue; // Check isolation of tagged muons (R<0.3)
       
+      for ( int i = 0; i < iMu.size(); i++ ) {pTmu.fill(pythia.event[iMu[i]].pT()); file << pythia.event[iMu[i]].pT() << endl;}
       for ( int i = 0; i < iE.size(); i++ ) {pTe.fill(pythia.event[iE[i]].pT()); file1 << pythia.event[iE[i]].pT() << endl;}
     }
 
     /* === single lepton trigger === */
-    if ( iE.size() > 0 && iEp.size() > 0 && iMup.size() == 0 ){ // Single electron
+    if ( iE.size() == 1 && iMu.size() == 0 ){ // Single electron
+      if ( pTelectrons[2] < 1 ) continue;
       cout << "single e" << endl; getchar();
-      if ( pTelectrons[2] < 1 ) continue;
     }
-    if ( iMu.size() > 0 && iMup.size() > 0 && iEp.size() == 0 ){ // Single muon
-      cout << "single mu" << endl; getchar();
+    if ( iMu.size() == 1 && iE.size() == 0 ){ // Single muon
       if ( pTelectrons[2] < 1 ) continue;
+      cout << "single mu" << endl; getchar();
     }
     
 
